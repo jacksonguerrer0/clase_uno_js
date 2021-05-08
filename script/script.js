d5e4c33d8b8166e8536e29a17c75b24f73ff7d5c
Jackson Guerrero <jacksonguerrerovega2@gmail.com>
2021-05-08 08:40:33 -0500

    Se termino el algoritmo para hallar el salario semanal, usando uba funcion y un condicional doble

--- script/script.js
+++ script/script.js
@@ -3,16 +3,21 @@ var salario = 0;
 var extra = 0;
 
 function Calcular(){
-    cantidad = document.getElementById('idCantidad').value;
+    cantidad = document.getElementById("idCantidad").value;
     console.log(cantidad);
 
-    if(cantidad >= 1 && cantidad <=40){
+    if(cantidad >= 1 && cantidad <= 40){
         salario = cantidad * 16000;
-        
+        document.getElementById("salario").value = salario;
+        document.getElementById("extra").value = extra;
+        document.getElementById("pay").value = salario;
     }
     else{
         extra = cantidad - 40;
-        salario = (extra*20000) + (40*16000);
+        salario = (extra * 20000) + (40*16000);
+        document.getElementById("salario").value = 40 * 16000;
+        document.getElementById("extra").value = extra * 20000;
+        document.getElementById("pay").value = salario;
     }
 
 }
